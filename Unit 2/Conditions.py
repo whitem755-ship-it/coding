4d3660a120cd4adaf8e9510ba3886891d661547b
whitem755-ship-it <whitem755@boyslatin.net>
2025-09-25 14:26:45 +0000

    not done

---
+++ Unit 2/Conditions.py
@@ -0,0 +1,58 @@
+# Conditional Statements = code instructions that
+# have different outcomes based on the inputted data.
+# input --> output
+
+# Conditional Statement Syntax
+# if keyword - controls the condition we want to satisfy.
+
+# else keyword - Our default outcome. The thing that
+# happens when our conditions are NOT satisfied
+
+
+Weather = input('what is the weather like today?')
+if Weather == 'sunny':
+    print("It is beautiful outside. Bring sunglasses.")
+elif Weather == 'rainy':
+    print(" Remember to bring an umbrella.")
+elif Weather == 'windy':
+    print("Wear heavy boots")
+elif Weather =='chilly':
+    print("bring a jacket.")
+else:
+    print("I cant tell you the weather, but have a good day.")
+
+
+Password = input('please enter password:')
+if Password == 'Boyslatin203':
+    print("Welcome you are logged in.")
+else:
+    print("Sorry try again")
+
+down = input('What down is it?')
+yards = input('How many yards do you need to get another first down?')
+
+if down == 1 and yards <= 5:
+    print("run it")
+elif down == 2 and yards <= 5:
+    print("run it")
+elif down == 3 and yards <= 5:
+    print("play action")
+else:
+    print("punt")
+
+def testTakerPermit(age):
+  if age > 16:
+      print("Congratulation you are old enough for your permit")
+  else:
+      print("You are not old enough for your permit")
+
+testTakerPermit(17)
+
+def etheirOr(Number):
+    if Number = +
+        print("this is positive")
+    else:
+        print("this number is negative")
+
+etheirOr(18)
+
